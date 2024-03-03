6d3e0bba49070a3d2c1b9d80d78c4df697dac622
Jun Bo Lau <23223857+junbolau@users.noreply.github.com>
2024-03-03 09:41:49 -0500

    Update magma_tiny_integrals.m

--- X_0_37/magma_tiny_integrals.m
+++ X_0_37/magma_tiny_integrals.m
@@ -1,3 +1,4 @@
+// From https://github.com/jtuitman/Coleman
 load "coleman.m";
 f := y^2 -( -x^6 - 9*x^4 -11*x^2 + 37);
 p := 3;
@@ -6,5 +7,7 @@ N := 15;
 data := coleman_data(f,p,N);
 Q := set_point(1,-4,data);
 R := set_point(-1,-4,data);
+
+// First two outputs are the integrals on holomorphic differentials, which agrees with our results
 coleman_integrals_on_basis(Q,R,data);
 
